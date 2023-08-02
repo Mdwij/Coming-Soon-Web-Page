726de313c7ed1b28871067869bfafdfea005c315
Mdwij <93826197+Mdwij@users.noreply.github.com>
2023-08-02 21:58:09 +0530

    Update comingsoon.js

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Jul 31, 2023 15:00:00").getTime();
+    const countDate = new Date("Aug 31, 2023 15:00:00").getTime();
 
     const currentTime = new Date().getTime();
     console.log(currentTime);
